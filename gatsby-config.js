8bea06852bf4c41797a9be07a4e9a04aa378ded6
Nupur Grover <nupur.grover@festicket.com>
2020-07-20 19:39:10 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Nupur's Personal Blog`,
+    author: `Nupur Grover`,
+    description: `A personal blog to talk about all things tech`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `gatsbyjs`,
